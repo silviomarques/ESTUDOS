bdc4f9ce4fb2d042a21999197467f53b504b3d39
Silvio Marques <silvio.nando@gmail.com>
2017-10-17 00:22:16 -0200

    todos
    atualizações

--- Chamados/src/ListarChamadosServlet.java
+++ Chamados/src/ListarChamadosServlet.java
@@ -11,12 +11,21 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 
 public class ListarChamadosServlet extends HttpServlet {
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter out = response.getWriter();
+		
+		HttpSession sessao = request.getSession();
+		
+		if(sessao.getAttribute("login") == null) {
+			response.sendRedirect("http://localhost:8080/Chamados/Login");
+		}
+		
+		out.println("<br><a href='http://localhost:8080/Chamados/Login?msg=logoff'>Sair</a>");
 		
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
